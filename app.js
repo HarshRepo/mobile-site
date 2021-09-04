29bb1db999859b9cea27f894447c0474ba8f1726
HarshRepo <amarmalvi999@gmail.com>
2021-09-05 01:10:55 +0530

    Signed-off-by: HarshRepo <amarmalvi999@gmail.com>

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mainRouter = require('./router/index.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
+
+app.use(mainRouter);
+
+
+
+
+
+app.listen(PORT , () => {
+    console.log(`app is on ${PORT}`)
+})
